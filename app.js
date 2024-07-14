e6aa80a490fb878c740654b4ee6895eb6aa4437f
Naser-Alzaghari <naseralzaghari88@gmail.com>
2024-07-14 21:53:27 +0300

    loops

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var userName = prompt("Enter you name:");
-var gender = prompt("Enter your gender: (male / female)");
+var gender =prompt("Enter your gender: (male / female)");
+while (gender != "male" && gender != "female"){
+    gender = prompt("Try again!\nEnter your gender: (male / female)");
+    
+}
 var welcomeMessage = "Welcome ";
 switch(gender){
     case "male": welcomeMessage=welcomeMessage.concat("Mr ");break;
@@ -7,20 +11,12 @@ switch(gender){
 }
 welcomeMessage=welcomeMessage.concat(userName);
 alert(welcomeMessage);
-var dount = confirm("do you want Dount?");
-var coffee = confirm("do you want Coffee?");
-var iceCream = confirm("do you want ice cream?");
-var bakery = confirm("do you want Bakery?");
+var typeOfOrder = prompt("what do you want to order? (Dount , Coffee , Ice cream , Bakery)");
 
 var orderName = prompt("Enter the name of the ordar:");
 
 alert('Your order "'+orderName+'" is getting prepared!');
-var statement = "";
-
-switch(gender){
-    case "male": statement=statement.concat("Mr ");break;
-    case "female": statement=statement.concat("Ms ");break;
+var info = [userName,gender,typeOfOrder,orderName];
+for (let i = 0; i<info.length;i++){
+    console.log(info[i]);
 }
-statement=statement + userName + " orderd " + (dount? "dount " : "") + (coffee? "coffee " : "") + (iceCream? "iceCream " : "") + (bakery? "bakery" : "");
-console.log(statement);
-
